f64d9cac4eb1faf46096690640c62f134dfce4eb
Muzaffar2807 <mmkhan2807@gmail.com>
2024-02-25 16:04:05 +0530

    footer

--- src/components/Layouts/Footer.jsx
+++ src/components/Layouts/Footer.jsx
@@ -1,54 +1,81 @@
-import React from 'react';
-import "./Footer.scss"
+import React from "react";
+import "./Footer.scss";
+import {
+  FaChevronRight,
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaYoutube,
+  FaPinterestP,
+  FaThumbsUp,
+} from "react-icons/fa";
 
 const Footer = () => {
   return (
     <>
-      <div>
-        <div>
+      <div className="footer-wrapper">
+        <div className="footer-div">
           <h2>USEFUL LINKS</h2>
           <ul>
-            <li>
-              <a href="#">Guest Policy</a>
-            </li>
-            <li>
-              <a href="#">Privacy Policy</a>
-            </li>
-            <li>
-              <a href="#">Cookie Policy</a>
-            </li>
-            <li>
-              <a href="#">T & C</a>
-            </li>
-            <li>
-              <a href="#">Careers</a>
-            </li>
-            <li>
-              <a href="#">FAQ's</a>
-            </li>
-            <li>
-              <a href="#"> About Us</a>
-            </li>
-            <li>
-              <a href="#">Contact Us</a>
-            </li>
+            <div className="links-container">
+              <div>
+                <li>
+                  <a href="#">Guest Policy</a>
+                </li>
+                <li>
+                  <a href="#">Privacy Policy</a>
+                </li>
+                <li>
+                  <a href="#">Cookie Policy</a>
+                </li>
+              </div>
+
+              <div>
+                <li>
+                  <a href="#">T & C</a>
+                </li>
+                <li>
+                  <a href="#">Careers</a>
+                </li>
+                <li>
+                  <a href="#">FAQ's</a>
+                </li>
+              </div>
+
+              <div>
+                <li>
+                  <a href="#"> About Us</a>
+                </li>
+                <li>
+                  <a href="#">Contact Us</a>
+                </li>
+              </div>
+            </div>
           </ul>
         </div>
 
-        <div>
+        <div className="subscribe-container">
           <h2>SUBSCRIBE</h2>
 
           <div className="form">
             <div>
               <div className="input-container">
-                <input type="text" placeholder="Full Name*" />
-                <input type="email" placeholder="Email*" />
+                <input
+                  type="text"
+                  placeholder="Full Name*"
+                  className="input-box"
+                />{" "}
+                <br />
+                <input
+                  type="email"
+                  placeholder="Email*"
+                  className="input-box"
+                />
               </div>
 
               <div className="button">
                 <button>
-                  {" "}
-                  <p> enter</p>
+                  <FaChevronRight color="#000" />
                 </button>
               </div>
             </div>
@@ -65,29 +92,48 @@ const Footer = () => {
           <h2>SOCIAL</h2>
 
           <div className="social-icons">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+            <div>
+              <FaFacebookF color="#000" />
+            </div>
+            <div>
+              <FaInstagram color="#000" />
+            </div>
+            <div>
+              <FaTwitter color="#000" />
+            </div>
+            <div>
+              <FaYoutube color="#000" />
+            </div>
+            <div>
+              <FaPinterestP color="#000" />
+            </div>
           </div>
 
           <div className="act-btns">
-            <button>Like</button>
-            <button>Share</button>
+            <button className="like-btn">
+              <FaThumbsUp />
+              Like
+            </button>
+            <button className="share-btn">
+              {" "}
+              <FaFacebookF /> Share
+            </button>
           </div>
 
-          <div>SOLES ON WHEELS © 2021 All Rights Reserved</div>
+          <div className="mt-8">
+            <p>SOLES ON WHEELS © 2021 </p>
+            <p> All Rights Reserved</p>
+          </div>
         </div>
       </div>
 
       <div className="bottom-footer">
         <a href="#">Destinations</a>
 
-        <a href="#">Covide-19 FAQ's</a>
+        <a href="#" className="pl-8">Covide-19 FAQ's</a>
       </div>
     </>
   );
-}
+};
 
-export default Footer
+export default Footer;
