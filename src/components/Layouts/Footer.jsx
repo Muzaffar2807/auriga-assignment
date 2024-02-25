1cb619f37ae6196cec551a03f2434427ea98fd1b
Muzaffar2807 <mmkhan2807@gmail.com>
2024-02-25 15:08:00 +0530

    header, footer

--- src/components/Layouts/Footer.jsx
+++ src/components/Layouts/Footer.jsx
@@ -3,8 +3,91 @@ import "./Footer.scss"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <>
+      <div>
+        <div>
+          <h2>USEFUL LINKS</h2>
+          <ul>
+            <li>
+              <a href="#">Guest Policy</a>
+            </li>
+            <li>
+              <a href="#">Privacy Policy</a>
+            </li>
+            <li>
+              <a href="#">Cookie Policy</a>
+            </li>
+            <li>
+              <a href="#">T & C</a>
+            </li>
+            <li>
+              <a href="#">Careers</a>
+            </li>
+            <li>
+              <a href="#">FAQ's</a>
+            </li>
+            <li>
+              <a href="#"> About Us</a>
+            </li>
+            <li>
+              <a href="#">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h2>SUBSCRIBE</h2>
+
+          <div className="form">
+            <div>
+              <div className="input-container">
+                <input type="text" placeholder="Full Name*" />
+                <input type="email" placeholder="Email*" />
+              </div>
+
+              <div className="button">
+                <button>
+                  {" "}
+                  <p> enter</p>
+                </button>
+              </div>
+            </div>
+
+            <div className="terms-conditions">
+              <input type="checkbox" />
+
+              <p>I accept Terms & Conditions</p>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <h2>SOCIAL</h2>
+
+          <div className="social-icons">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+
+          <div className="act-btns">
+            <button>Like</button>
+            <button>Share</button>
+          </div>
+
+          <div>SOLES ON WHEELS © 2021 All Rights Reserved</div>
+        </div>
+      </div>
+
+      <div className="bottom-footer">
+        <a href="#">Destinations</a>
+
+        <a href="#">Covide-19 FAQ's</a>
+      </div>
+    </>
+  );
 }
 
 export default Footer
--- src/components/Layouts/Header.jsx
+++ src/components/Layouts/Header.jsx
@@ -1,29 +1,54 @@
 import { Fragment, useState } from "react";
 import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
 import {
-  ArrowPathIcon,
   Bars3Icon,
   ChartPieIcon,
   CursorArrowRaysIcon,
-  FingerPrintIcon,
-  SquaresPlusIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline";
-import {
-  ChevronDownIcon,
-  PhoneIcon,
-  PlayCircleIcon,
-} from "@heroicons/react/20/solid";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import logo from "../../assets/images/Logo FINAL png.webp";
-import { IoMdLogIn } from "react-icons/io";
+import { MdAccountCircle } from "react-icons/md";
 
-import categories from "../../assets/data/menuData";
 import "./Header.scss";
-import menu from "../../assets/data/menuData";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
+
+const homeDropdown = [
+  {
+    name: "Munnar",
+    description: "It's the place you would love to visit!",
+    href: "#",
+    icon: ChartPieIcon,
+  },
+  {
+    name: "Vaganum",
+    description:
+      "Vagamon is a paradise for travellers with its mystical weather",
+    href: "#",
+    icon: CursorArrowRaysIcon,
+  },
+];
+
+const moreDropdown = [
+  {
+    name: "Members",
+
+    href: "#",
+  },
+  {
+    name: "Forum",
+
+    href: "#",
+  },
+  {
+    name: "Groups",
+
+    href: "#",
+  },
+];
 
 export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -31,7 +56,7 @@ export default function Example() {
   return (
     <header className="bg-white fixed top-0 w-full z-50 shadow-md">
       <nav
-        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        className="mx-auto flex max-w-7xl items-center justify-between p-4 lg:px-4"
         aria-label="Global"
       >
         <div className="flex lg:flex-1 logo-container">
@@ -41,9 +66,10 @@ export default function Example() {
                 <img className="h-9 w-auto" src={logo} alt="Auriga" />
               </div>
               <div className="text-div">
-                <h1 className="">SOLES ON Wheels</h1>
-
-                <p className="text2">The Backpacker's Hostel</p>
+                <h1 style={{ fontFamily: "logo-font", color: "#324158" }}>
+                  SOLES ON Wheels
+                </h1>
+                <p className="text2 pr-4">The Backpacker's Hostel</p>
               </div>
             </div>
           </a>
@@ -58,32 +84,132 @@ export default function Example() {
             <Bars3Icon className="h-6 w-6" aria-hidden="true" />
           </button>
         </div>
-        <Popover.Group className="hidden lg:flex lg:gap-x-12">
-          <a href="#" className="text-md font-bold leading-6 text-gray-900 ">
-            Home
+        <Popover.Group className="hidden lg:flex lg:gap-x-12 items-center listItems">
+          <a href="#" className="text-sm   mt-1 leading-6 text-gray-900 ">
+            <p className="px-1 py-0 mt-2 align-middle">Home</p>
           </a>
-          <a href="#" className="text-md font-bold leading-6 text-gray-900 ">
-            Hostels
+
+          <Popover className="relative">
+            <Popover.Button
+              style={{ fontFamily: "logo-text-font" }}
+              className="flex     items-center gap-x-1 text-sm  leading-6 text-gray-900"
+            >
+              <a href="#" className="text-sm  mt-1  leading-6 text-gray-900 ">
+                <p className=" py-0 mt-2 ">Hostels</p>
+              </a>
+              <ChevronDownIcon
+                className="h-4 w-4 mb-1 flex-none text-gray-400"
+                aria-hidden="true"
+              />
+            </Popover.Button>
+
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-2xl bg-white shadow-lg ring-1 ring-gray-900/5">
+                <div className="p-4">
+                  {homeDropdown.map((item) => (
+                    <div
+                      key={item.name}
+                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
+                    >
+                      <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                        <item.icon
+                          className="h-6 w-6 text-gray-600 group-hover:text-indigo-600"
+                          aria-hidden="true"
+                        />
+                      </div>
+                      <div className="flex-auto">
+                        <a
+                          href="#"
+                          className="block font-semibold text-gray-900"
+                        >
+                          {item.name}
+                          <span className="absolute inset-0" />
+                        </a>
+                        <p className="mt-1 text-gray-600">{item.description}</p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </Popover.Panel>
+            </Transition>
+          </Popover>
+
+          <a href="#" className="text-sm   mt-1 leading-6 text-gray-900 ">
+            <p className="px-1 py-0 mt-2 ">About</p>
           </a>
-          <a href="#" className="text-md font-bold leading-6 text-gray-900 ">
-            About us
+          <a href="#" className="text-sm  mt-1 leading-6 text-gray-900 ">
+            <p className="px-1 py-0 mt-2 ">FAQ's </p>
           </a>
-          <a href="#" className="text-md font-bold leading-6 text-gray-900 ">
-            Career
+          <a href="#" className="text-sm  mt-1  leading-6 text-gray-900 ">
+            <p className="px-1 py-0 mt-2 ">Careers</p>
           </a>
-          <a href="#" className="text-md font-bold leading-6 text-gray-900 ">
-            More
+          <a href="#" className="text-sm   mt-1 leading-6 text-gray-900 ">
+            <p className="px-1 py-0 mt-2 ">Contact</p>
           </a>
+          <Popover className="relative">
+            <Popover.Button
+              style={{ fontFamily: "logo-text-font" }}
+              className="flex     items-center gap-x-1 text-sm  leading-6 text-gray-900"
+            >
+              <a href="#" className="text-sm  mt-1  leading-6 text-gray-900 ">
+                <p className="  py-0 mt-2 ">More</p>
+              </a>
+              <ChevronDownIcon
+                className="h-4 w-4 mb-1 flex-none text-gray-400"
+                aria-hidden="true"
+              />
+            </Popover.Button>
+
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel className="absolute -left-8 w-40 top-full z-10 mt-3   max-w-md overflow-hidden rounded-2xl bg-white shadow-lg ring-1 ring-gray-900/5">
+                <div className="p-4">
+                  {moreDropdown.map((item) => (
+                    <div
+                      key={item.name}
+                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
+                    >
+                      <div className="flex-auto">
+                        <a
+                          href="#"
+                          className="block font-semibold text-gray-900"
+                        >
+                          {item.name}
+                          <span className="absolute inset-0" />
+                        </a>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </Popover.Panel>
+            </Transition>
+          </Popover>
         </Popover.Group>
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+        <div className="hidden lg:flex lg:flex-1 lg:justify-center items-center">
+          <a href="#">
+            <MdAccountCircle size={30} />
+          </a>
           <a
             href="#"
-            className="text-md font-semibold leading-6 text-gray-900 pr-3 text-base"
+            className="text-sm   leading-6 text-gray-900 pl-2 text-center  mt-2"
+            style={{ fontFamily: "logo-text-font", textAlign: "center" }}
           >
-            Log in
-          </a>
-          <a href="#">
-            <IoMdLogIn size={26} />
+            <p>Log In</p>
           </a>
         </div>
       </nav>
@@ -118,39 +244,84 @@ export default function Example() {
               <div className="space-y-2 py-6">
                 <a
                   href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base   leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   Home
                 </a>
+                <Disclosure as="div" className="-mx-3">
+                  {({ open }) => (
+                    <>
+                      <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                        Hostels
+                        <ChevronDownIcon
+                          className={classNames(
+                            open ? "rotate-180" : "",
+                            "h-5 w-5 flex-none"
+                          )}
+                          aria-hidden="true"
+                        />
+                      </Disclosure.Button>
+                      <Disclosure.Panel className="mt-2 space-y-2">
+                        {[...homeDropdown].map((item) => (
+                          <Disclosure.Button
+                            key={item.name}
+                            as="a"
+                            href={item.href}
+                            className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                          >
+                            {item.name}
+                          </Disclosure.Button>
+                        ))}
+                      </Disclosure.Panel>
+                    </>
+                  )}
+                </Disclosure>
+
                 <a
                   href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Hostels
-                </a>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base   leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   About us
                 </a>
                 <a
                   href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base   leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   Careers
                 </a>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  More
-                </a>
+                <Disclosure as="div" className="-mx-3">
+                  {({ open }) => (
+                    <>
+                      <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                        More
+                        <ChevronDownIcon
+                          className={classNames(
+                            open ? "rotate-180" : "",
+                            "h-5 w-5 flex-none"
+                          )}
+                          aria-hidden="true"
+                        />
+                      </Disclosure.Button>
+                      <Disclosure.Panel className="mt-2 space-y-2">
+                        {[...moreDropdown].map((item) => (
+                          <Disclosure.Button
+                            key={item.name}
+                            as="a"
+                            href={item.href}
+                            className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                          >
+                            {item.name}
+                          </Disclosure.Button>
+                        ))}
+                      </Disclosure.Panel>
+                    </>
+                  )}
+                </Disclosure>
               </div>
               <div className="py-6">
                 <a
                   href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base   leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   Log in
                 </a>
